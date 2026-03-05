62abc6db6ad409b17d43840443a5c8c56b7464a4
Zechariah <zechariahadebayo42@gmail.com>
2026-03-05 15:24:05 +0100

    chore: frontend config update again

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/auths/login.tsx';
+import Signup from './components/auths/signup.tsx';
 
 
 const App = () => {
@@ -7,6 +9,8 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={'Home page in the app file'} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/meeting/:id" element={`Meeting page for ID: ${window.location.pathname.split('/').pop()}`} />
       </Routes>
     </Router>
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -72,35 +72,3 @@ module.exports = {
         extensions: [".js", ".jsx", ".ts", ".tsx"],
     }
 }
-
-
-// import path from "node:path";
-// import { fileURLToPath } from "url";
-// import webpack from "webpack";
-
-// import "webpack-dev-server";
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-
-// const config: webpack.Configuration = {
-//   entry: "./src/index.ts",
-//   module: {
-//     rules: [
-//       {
-//         test: /\.tsx?$/,
-//         use: "ts-loader",
-//         exclude: /node_modules/,
-//       },
-//     ],
-//   },
-//   resolve: {
-//     extensions: [".tsx", ".ts", ".js"],
-//   },
-//   output: {
-//     filename: "bundle.js",
-//     path: path.resolve(__dirname, "dist"),
-//   },
-// };
-
-// export default config;
